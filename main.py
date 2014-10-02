5ee9adb4760673ab02015f14835a729e19716bae
annikaC <annika.clarke@gmail.com>
2014-10-02 20:39:27 +0100

    Add cities and drunk words

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ ACCESS_TOKEN = twitter.obtain_access_token()
 
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
+words = ['beer',]
+
+def get_lats_longs():
+    twitter.search_geo(
+        query='London',
+    )
+
 
 def how_drunk():
     tweets = twitter.search(
         q='-RT',
         geocode='51,0,10mi',
         result_type='recent',
-        count='100')
+        count='5')
 
     for tweet in tweets['statuses']:
-        print tweet['text']
+        print tweet
 
 if __name__ == "__main__":
     how_drunk()
